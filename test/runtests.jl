73eef02a43000ef0e12085c9b55ce0d6bb296e53
Thomas Baruchel <baruchel@gmx.com>
2016-03-06 10:41:30 +0100

    Try to fix tests for Julia v.0.3

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,8 @@ using Base.Test
 
 @test recvec([1,2,3,4,5,6,7,8,9,10]) == [1,-2,1]
 @test recvec([0,1,1,2,3,5,8,13], output=Float64) == [1.0,-1.0,-1.0]
-@test recvec([1,2,4,8,16,32,64,128], compute=Int128) == [1,-2]
-@test recvec([1,1//2,1//4,1//8,1//16,1//32,1//64,1//128], compute=Int128,
-         output=Float64) == [2.0,-1.0]
+@test recvec([2^k for k=0:7], compute=Int64) == [1,-2]
+@test recvec([1//2^k for k=0:7], compute=Int64, output=Float64) == [2.0,-1.0]
 @test recvec([1]) == []
 @test recvec([1,2,3,4,5,6,7,8,9,10,42]) == []
 @test recvec([BigInt(2)^k + k%16 for k=1:64]) == 
